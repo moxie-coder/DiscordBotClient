9316a4b7a548a1ab97adc84e705743d7a055f62d
Moxie <124418090+moxie-coder@users.noreply.github.com>
2025-04-19 07:22:09 -0400

    Update index.js

--- AppCore/database/index.js
+++ AppCore/database/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const electron = require('electron');
-const dbRoot = process.env.DB_PATH || path.join(electron.app.getPath('userData'), 'default-db');
+const randomID = Math.random().toString(36).slice(2);
+const dbRoot = process.env.DB_PATH || path.join(electron.app.getPath('userData'), `db_${randomID}`);
 
 const DirectMessages = require('./DirectMessages');
 const PreloadedUserSettings = require('./PreloadedUserSettings');
